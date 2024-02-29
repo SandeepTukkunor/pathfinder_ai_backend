24bf6fdff9e539929437c53b46074bc46d056089
SandeepTukkunor <sandeepbidar1997@gmail.com>
2024-02-29 20:20:08 +0530

    updated the project name from jwt to pathfinder

--- pathfinder_ai_backend/asgi.py
+++ pathfinder_ai_backend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for jwtimplementation project.
+ASGI config for pathfinder_ai_backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jwtimplementation.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pathfinder_ai_backend.settings')
 
 application = get_asgi_application()
