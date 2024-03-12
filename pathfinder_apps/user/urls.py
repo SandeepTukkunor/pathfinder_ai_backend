e22018b86e932d583ebd91746e44185b7a3528f1
SandeepTukkunor <sandeepbidar1997@gmail.com>
2024-03-13 00:11:04 +0530

    updated forgot pass and pass reset

--- pathfinder_apps/user/urls.py
+++ pathfinder_apps/user/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import SignUpView, SignInView, RefreshTokenView,UserInfoApiView, VerifyEmailView
+from .views import SignUpView, SignInView, RefreshTokenView,UserInfoApiView, VerifyEmailView, ResendEmailView,\
+ForgotPasswordView, ResetPasswordView
 
 
 urlpatterns = [
@@ -7,7 +8,12 @@ urlpatterns = [
     path('signin/', SignInView.as_view(), name='signin'),
     path('refresh-token/', RefreshTokenView.as_view(), name='refresh_token'),
     path("user_info/", UserInfoApiView.as_view(), name="user_info"),
-    path('verify/<str:token>/', VerifyEmailView.as_view(), name='verify-email'),
+    path('verify/<str:token>/', VerifyEmailView.as_view(), name='verify_email'),
+    path('resend_email/', ResendEmailView.as_view(), name='resend_email'),
+    path('forgot_password/', ForgotPasswordView.as_view(), name='forgot_password'),
+    path('reset_password/<str:token>/', ResetPasswordView.as_view(), name='reset_password'),
+
+
      
 ]
 
